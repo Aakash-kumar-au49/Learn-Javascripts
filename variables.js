ae7a955d15fa339481fb910a36b6c9b15aae1ef8
Aakash Kumar <109100208+Aakash-kumar-au49@users.noreply.github.com>
2023-12-30 09:33:38 +0000

    start

---
+++ variables.js
@@ -0,0 +1,19 @@
+const myId = 251203
+let myEmail = "saini@gmail.com"
+var myPassword = "123456"
+myCity = "Myzaffarnagar"
+
+
+//myId = 301030  not Allowed because Id is constant
+
+myEmail = "akash@.com"
+myPassword = "121212"
+myCity = "meerut"
+
+console.table([myId, myEmail, myPassword, myCity]);
+
+console.log(myId)
+
+/* prefer not to use var
+   beacause of issue in block space and functional scope 
+*/
